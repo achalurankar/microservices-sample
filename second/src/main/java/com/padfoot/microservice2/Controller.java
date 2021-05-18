702aa2f92cce8c3a8205cf20f5f1da2be3f96102
Aochocinco <achalurankar@gmail.com>
2021-05-18 20:57:25 +0530

    first commit

---
+++ second/src/main/java/com/padfoot/microservice2/Controller.java
@@ -0,0 +1,19 @@
+package com.padfoot.microservice2;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/microservice/v1")
+public class Controller {
+	
+	@GetMapping(value = "/string")
+	public ResponseEntity<?> getAddress() throws Exception  {
+		
+		return new ResponseEntity<>("string from microservice 2",HttpStatus.OK);
+
+	}
+}
